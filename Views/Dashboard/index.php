0faa67aa1c178aeebff1a58468ace246f3b2a7cd
kartmann17 <dronex.contact@gmail.com>
2024-11-26 15:15:40 +0100

    ajout css navbar et footer et css dashboard ainsi que page index dashboard

---
+++ Views/Dashboard/index.php
@@ -0,0 +1,130 @@
+<link rel="stylesheet" href="Asset/css/dashboard.css">
+<div class="container-fluid admin-container">
+    <div class="admin-space">
+        <h2 class="mb-4 text-center">Dashboard Admin</h2>
+        <div class="dashboard-container">
+            <div class="admin-link-container">
+                <a href="/DashBurgers" class="admin-link">Ajout Burger</a>
+            </div>
+            <div class="admin-link-container">
+                <a href="/DashBurgers/liste" class="admin-link">Liste Burgers</a>
+            </div>
+            <div class="admin-link-container">
+                <a href="/DashTacos" class="admin-link">Ajout Tacos</a>
+            </div>
+            <div class="admin-link-container">
+                <a href="/DashTacos/liste" class="admin-link">Liste Tacos</a>
+            </div>
+
+            <!-- Ajout Viande -->
+           <div class="admin-link-container">
+                <a href="/DashViande" class="admin-link">Ajout Viande</a>
+            </div>
+
+            <!-- Liste Viande -->
+           <div class="admin-link-container">
+                <a href="/DashViande/liste" class="admin-link">Liste Viandes</a>
+            </div>
+
+            <!-- Ajout Sauces -->
+           <div class="admin-link-container">
+                <a href="/DashSauce" class="admin-link">Ajout Sauces</a>
+            </div>
+
+            <!-- Liste Sauces -->
+           <div class="admin-link-container">
+                <a href="/DashSauce/liste" class="admin-link">Liste Sauces</a>
+            </div>
+
+            <!-- Ajout Kebab -->
+           <div class="admin-link-container">
+                <a href="/DashKebabs" class="admin-link">Ajout Kebab</a>
+            </div>
+
+            <!-- Liste Kebab -->
+           <div class="admin-link-container">
+                <a href="/DashKebabs/liste" class="admin-link">Liste Kebabs</a>
+            </div>
+
+            <!-- Ajout Salades -->
+           <div class="admin-link-container">
+                <a href="/DashSalade" class="admin-link">Ajout Salades</a>
+            </div>
+
+            <!-- Liste Salades -->
+           <div class="admin-link-container">
+                <a href="/DashSalade/liste" class="admin-link">Liste Salades</a>
+            </div>
+
+            <!-- Ajout Snack -->
+           <div class="admin-link-container">
+                <a href="/DashSnack" class="admin-link">Ajout Snack</a>
+            </div>
+
+            <!-- Liste Snack -->
+           <div class="admin-link-container">
+                <a href="/DashSnack/liste" class="admin-link">Liste Snacks</a>
+            </div>
+
+             <!-- Ajout Snack -->
+           <div class="admin-link-container">
+                <a href="/DashSupplements" class="admin-link">Ajout Supp</a>
+            </div>
+
+            <!-- Liste Snack -->
+           <div class="admin-link-container">
+                <a href="/DashSupplements/liste" class="admin-link">Liste Supplement</a>
+            </div>
+
+            <!-- Ajout Soft -->
+           <div class="admin-link-container">
+                <a href="/DashSofts" class="admin-link">Ajout Soft</a>
+            </div>
+
+            <!-- Liste Soft -->
+           <div class="admin-link-container">
+                <a href="/DashSofts/liste" class="admin-link">Liste Softs</a>
+            </div>
+
+            <!-- Ajout Vins -->
+           <div class="admin-link-container">
+                <a href="/DashVins" class="admin-link">Ajout Vins</a>
+            </div>
+
+            <!-- Liste Vins -->
+           <div class="admin-link-container">
+                <a href="/DashVins/liste" class="admin-link">Liste Vins</a>
+            </div>
+
+            <!-- Ajout Bière -->
+           <div class="admin-link-container">
+                <a href="/DashBieres" class="admin-link">Ajout Bière</a>
+            </div>
+
+            <!-- Liste Bières -->
+           <div class="admin-link-container">
+                <a href="/DashBieres/liste" class="admin-link">Liste Bières</a>
+            </div>
+
+            <!-- Ajout Horaire -->
+           <div class="admin-link-container">
+                <a href="/DashHoraire" class="admin-link">Ajout Horaire</a>
+            </div>
+
+            <!-- Liste Horaires -->
+           <div class="admin-link-container">
+                <a href="/DashHoraire/liste" class="admin-link">Liste Horaires</a>
+            </div>
+
+            <!-- Validation Avis -->
+           <div class="admin-link-container">
+                <a href="/DashValideAvis" class="admin-link">Validation Avis</a>
+            </div>
+
+            <!-- Liste Avis -->
+           <div class="admin-link-container">
+                <a href="/DashValideAvis/liste" class="admin-link">Liste Avis</a>
+            </div>
+        </div>
+    </div>
+</div>
