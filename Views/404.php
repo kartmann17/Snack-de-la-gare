cea206064bad72df0768a589ae8587fea313d18d
kartmann17 <dronex.contact@gmail.com>
2024-11-26 22:18:19 +0100

    ajout index racine public and other pong js and 404 not found

---
+++ Views/404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Asset/css/404.css">
+    <title>404 - Page non trouvée</title>
+</head>
+
+<body>
+<div class="game-container">
+        <h1>404 - Page non trouvée</h1>
+        <p>Oups ! Cette page n'existe pas. En attendant, amusez-vous avec ce jeu de Pong !</p>
+        <canvas id="gameCanvas" width="600" height="400"></canvas>
+        <script src="/Asset/Js/pong.js"></script>
+    </div>
+
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Autoloader;
+use App\Config\Main;
+use Dotenv\Dotenv;
+
+// Inclure l'autoloader de Composer
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Définition d'une constante avec le chemin racine du projet
+define('ROOT', dirname(__DIR__));
+
+require_once ROOT . '/Autoloader.php';
+Autoloader::register();
+
+$app = new Main();
+
+$app->start();
