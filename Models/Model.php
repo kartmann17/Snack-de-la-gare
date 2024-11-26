a66c6fa8077460401d5d65d350c822d0c37db6fd
kartmann17 <dronex.contact@gmail.com>
2024-11-26 14:42:07 +0100

    ajout model principal

---
+++ Models/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+class Model
+{
+
+    public function hydrate($data)
+    {
+        foreach ($data as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+        return $this;
+    }
+}
