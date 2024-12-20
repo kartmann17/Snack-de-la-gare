363330b76e6ee5a73749d128b26a9888a69214a4
kartmann17 <dronex.contact@gmail.com>
2024-12-20 10:48:31 +0100

    modification index

--- public/index.php
+++ public/index.php
@@ -7,10 +7,9 @@ use Dotenv\Dotenv;
 // Inclure l'autoloader de Composer
 require_once __DIR__ . '/../vendor/autoload.php';
 
-if (file_exists(__DIR__ . '/../.env')){
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
-}
+
 // Définition d'une constante avec le chemin racine du projet
 define('ROOT', dirname(__DIR__));
 
